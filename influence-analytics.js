3b71bac3b2d141a8e87ffc572ace26fba0c49aea
Maek Twain <saranshisat@icloud.com>
2018-04-18 05:05:30 +0530

    Notif

--- influence-analytics.js
+++ influence-analytics.js
@@ -18,6 +18,16 @@ if (typeof Influence === 'undefined') {
 
         tracker = new InfluenceTracker(options.trackingId);
 
+        /**
+         * New InfluenceNotification()
+         * @type {{Notifications}}
+         */
+
+        notifications = new Notifications(options.trackingId);
+
+        this.notificationsInstance = notifications;
+
+
         options = options || {};
 
         this.options    = options;
@@ -3813,12 +3823,40 @@ if (typeof Influence === 'undefined') {
     })(Influence);
 }
 
+
+
 var InfluenceTracker = function(config) {
     if (!(this instanceof InfluenceTracker)) return new InfluenceTracker(config);
 
     this.config = config;
 
     console.log(config);
+
+};
+
+var Notifications = function(config) {
+  if (!(this instanceof Notifications)) return new Notifications(config);
+  this.config = config;
+  console.log("This is notifications " + config);
+
+};
+
+
+Notifications.prototype.notifications = function(info) {
+
+    /**
+    **Here all the action related to the notifications
+    */
+
+    /** Basic idea is to implement the toasterJS here so, that
+    /** WE fetch from the API say /sendSomeNotification?trackingId=INF-XXXXX;
+    **  We get Some Notifications and then we use toastrJs to display them/
+    **/
+  console.log(info)
+
+  var note = new Note({});
+  note.info("Hey!", "I'm leaving in 5 seconds", { duration: 5 });
+
 
 };
 
@@ -3847,7 +3885,7 @@ InfluenceTracker.prototype.tracker = function(info) {
                console.log("WebSocket is supported by your Browser!");
 
                // Let us open a web socket
-               var ws = new WebSocket("wss://strapi.useinfluence.co/web");
+               var ws = new WebSocket("ws://localhost/web");
 
                ws.onopen = function()
                {
@@ -3886,12 +3924,124 @@ InfluenceTracker.prototype.tracker = function(info) {
     }
 };
 
-//Basic idea is to implement the toasterJS here so, that
-/**WE fetch from the API say /sendSomeNotification?trackingId=INF-XXXXX;
-**  We get Some Notifications and then we use toastrJs to display them/
-**/
+var Note = function Note(config) {
+    var self = this;
+
+    function createElement(node, className, content) {
+        var e = document.createElement(node);
+        if (className) e.className = className;
+
+        if (content) {
+            if (typeof content === "string") e.innerHTML = content;else if (content.nodeName) e.appendChild(content);
+        }
+
+        return e;
+    }
+
+    function init() {
+        parseConfig();
+        build();
+    }
+
+    function parseConfig() {
+        self.config = {
+            duration: 4,
+            position: "bottomRight",
+            closeIcon: '<svg viewbox="0 0 40 40"><path d="M 10,10 L 30,30 M 30,10 L 10,30" /></svg>'
+        };
+
+        if (config) for (var opt in config) {
+            self.config[opt] = config[opt];
+        }
+    }
+
+    function build() {
+        self.container = createElement("div", "note--container " + self.config.position);
+        self.innerContainer = createElement("div", "note--inner");
+        self.container.appendChild(self.innerContainer);
+        document.body.appendChild(self.container);
+    }
+
+    function destroy() {
+        document.body.removeChild(self.container);
+    }
+
+    function showGeneric(type, title, content, noteConfig) {
+        // {
+        //     type: ["info", "success", "error", "warning"],
+        //     title: "",
+        //     content: ""
+        // }
+
+        noteConfig = noteConfig || {};
+
+        var note = createElement("div", "note shown note--" + type);
+        var noteFragment = document.createDocumentFragment(),
+            noteCloseButton = createElement("div", "note--close", self.config.closeIcon),
+            noteContent = createElement("div", "note--content");
+
+        if (title) {
+            note.classList.add("hasTitle");
+            noteContent.appendChild(createElement("h2", "note--title", title));
+        }
+
+        noteContent.appendChild(createElement("p", "note--body", content));
+
+        noteFragment.appendChild(noteContent);
+        noteFragment.appendChild(noteCloseButton);
+
+        note.appendChild(noteFragment);
+        self.innerContainer.appendChild(note);
+
+        var onAnimationEnd = function onAnimationEnd(e) {
+            return !note.classList.contains("shown") && self.innerContainer.removeChild(note);
+        };
+        var hide = function hide() {
+            note.classList.remove("shown");
+        };
+
+        note.addEventListener("click", hide);
+        note.addEventListener("animationend", onAnimationEnd);
+
+        if (!noteConfig.sticky && !self.config.sticky) setTimeout(hide, (noteConfig.duration || self.config.duration) * 1000);
+
+        return {
+            hide: hide,
+
+            show: function show() {
+                if (!note.parentNode) self.innerContainer.appendChild(note);
+                note.classList.add("shown");
+            }
+        };
+    }
+
+    init();
+
+    return {
+        show: showGeneric,
+        success: function success(title, text, config) {
+            return showGeneric("success", title, text, config);
+        },
+        info: function info(title, text, config) {
+            return showGeneric("info", title, text, config);
+        },
+        notify: function notify(title, text, config) {
+            return showGeneric("default", title, text, config);
+        },
+        error: function error(title, text, config) {
+            return showGeneric("error", title, text, config);
+        },
+        warn: function warn(title, text, config) {
+            return showGeneric("warning", title, text, config);
+        },
+        destroy: destroy
+    };
+};
 
 
 
+
+
+if (typeof module !== "undefined" && module.exports) module.exports = Note;
 nodeunit = typeof nodeunit === 'undefined' ? require('nodeunit') : nodeunit;
 Influence = typeof Influence === 'undefined' ? require('..') : Influence;
